bbc498ae4445917a4430eef13f24951520020a77
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2023-06-14 00:58:12 +0300

    Init project

---
+++ src/main/java/team/project/mriya/MriyaApplication.java
@@ -0,0 +1,13 @@
+package team.project.mriya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MriyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MriyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/project/mriya/MriyaApplicationTests.java
@@ -0,0 +1,13 @@
+package team.project.mriya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MriyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
